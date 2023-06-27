9a1e3174cff968407768a2948a7eb25e1386d59a
Yogesh Sherawat <yogeshwar.sherawat@kukufm.com>
2023-06-27 22:52:45 +0530

    Count it Out app complete

--- CountItOut/ViewController.swift
+++ CountItOut/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    @IBOutlet weak var counterLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickButtonPressed(_ sender: UIButton) {
+        count = count + 1
+        updateUI()
+    }
+    
+    func updateUI(){
+        counterLabel.text = String(count)
+    }
+    
 }
 
